dc1e535d431d474eee7d40dc660aaaa13df25659
Zhaoyu Guo <zhaoyu.guo@mail.utoronto.ca>
2021-07-01 19:18:43 -0400

    added storybook integration

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../src/Theme/BaseTheme";
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ChakraProvider theme={theme}>
+      <Story />
+    </ChakraProvider>
+  ),
+];
